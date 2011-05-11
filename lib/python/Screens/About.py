39d047a3c53a82616a66120128e855589810f0d9
meo <amedeo@localhost.localdomain>
2011-05-11 19:23:21 +0200

    add bhversion in about screen

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -12,8 +12,13 @@ class About(Screen):
 		Screen.__init__(self, session)
 
 		self["EnigmaVersion"] = StaticText("Enigma: " + about.getEnigmaVersionString())
-		self["ImageVersion"] = StaticText("Image: " + about.getImageVersionString())
-
+#		self["ImageVersion"] = StaticText("Image: " + about.getImageVersionString())
+		bhVer = "Blackhole"
+		f = open("/etc/bhversion",'r')
+		bhVer = f.readline().strip()
+		f.close()
+		self["ImageVersion"] = StaticText("Image: " + bhVer)
+		
 		self["TunerHeader"] = StaticText(_("Detected NIMs:"))
 
 		fp_version = getFPVersion()
