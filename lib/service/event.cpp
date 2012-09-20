b91b144d5fc345e62eaa4fa9c91100ab6223493b
meo <lupomeo@gmail.com>
2012-09-20 09:44:32 +0200

    event.cpp: remove "Visible gratis ..."

--- lib/service/event.cpp
+++ lib/service/event.cpp
@@ -92,7 +92,7 @@ bool eServiceEvent::loadLanguage(Event *evt, std::string lang, int tsidonid)
 					m_short_description = convertDVBUTF8(sed->getText(), table, tsidonid);
 //BlackHole
 					int mypos;
-					mypos = m_event_name.find("visibile gratis");
+					mypos = m_event_name.find("Visibile gratis");
 					if(mypos!=std::string::npos)
 						m_event_name=m_short_description;
 //End
