c25a7427b7054d60ecc05683043c0835b082cfdb
meo <lupomeo@hotmail.com>
2013-12-13 11:25:19 +0100

    BhSpeedUp add wmv support for solo duo and uno

--- lib/python/Blackhole/BhSettings.py
+++ lib/python/Blackhole/BhSettings.py
@@ -1415,6 +1415,8 @@ class BhSpeedUp(Screen, ConfigListScreen):
 			self.pluglist.append(["Web Manual (5.2 Mega)", "vuplus-manual"])
 		if machine != "vusolo":
 			self.pluglist.append(["HbbTV", "enigma2-plugin-extensions-hbbtv"])
+		if machine == "vusolo" || machine == "bm750" machine == "vuuno":
+			self.pluglist.append(["Wmv, Wma, Asf media support", "gst-ffmpeg"])
 		
 		self.activityTimer = eTimer()
 		self.activityTimer.timeout.get().append(self.updateFeed2)
